a28b98f354d6799420598902d7ab3b9d65f1b708
Jordan Schneringer <105464327+jschneringer@users.noreply.github.com>
2022-10-17 14:44:51 -0700

    Create number.py

---
+++ number.py
@@ -0,0 +1,11 @@
+#Variable that allows integer input
+
+number = int(input("Which number do you want to check? "))
+
+
+#if else logic
+
+if number % 2 == 0:
+    print("This is an even number.")
+else: 
+    print("This is an odd number.")
